09d78908d52e41106b5f0e82da55b28a44d784b7
Olga Kozhina <kozhina.olga.1997@gmail.com>
2018-03-14 04:14:17 +0400

    deleting the useless folder

--- annotation_practice/tasks/MyAnno.java
+++
@@ -1,9 +0,0 @@
-package annotation_practice.tasks;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface MyAnno {
-    String str();
-}
